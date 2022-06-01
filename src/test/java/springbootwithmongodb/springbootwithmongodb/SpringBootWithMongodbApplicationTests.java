22d382ad702cb72bd671fb57dec43873ffe25912
chathura <chathurabimalka@gmail.com>
2022-06-01 21:15:24 +0530

    first commit

---
+++ src/test/java/springbootwithmongodb/springbootwithmongodb/SpringBootWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootwithmongodb.springbootwithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
